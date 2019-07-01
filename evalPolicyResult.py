afa3cad85685470eca3c5d8b51d751def282cfab
Simon Bronner <simon@bronner.network>
2019-07-01 22:32:18 +1000

    Improved test output messaging

--- evalPolicyResult.py
+++ evalPolicyResult.py
@@ -3,24 +3,25 @@
 import json, sys
 from junit_xml import TestSuite, TestCase
 
-def generate_junit_report(failed_checks):
+def generate_junit_report(checks):
     test_cases = []
-    for (k, v) in failed_checks.items():
-        tc = TestCase(k, k, 0, "Policy Check Result: {}".format(v))
-        tc.add_failure_info("Policy [{}] check failed.".format(k), "failure")
+    for (k, v) in checks:
+        tc = TestCase(k, "policy", 0)
+        if (v == False):
+            tc.add_failure_info("Policy [{}] check failed.".format(k))
         test_cases.append(tc)
     return [TestSuite("Policy Checks", test_cases)]
 
 with open('result.json') as json_file:
     data = json.load(json_file)
-    failed_checks = {k: v for (k, v) in data[0].items() if v == False}
+    checks = data[0].items()
+    failed_checks = {k: v for (k, v) in checks if v == False}
     if len(failed_checks) > 0:
-        print("Policy Check Failures Detected!")
-        print("Failed policy checks:")
+        print("Policy Check Failures Detected! The following policy checks failed:")
         for (k, v) in failed_checks.items():
-            print("{}".format(k))
+            print("\t{}".format(k))
         with open('test-policy-result-report.xml', 'w') as f:
-            TestSuite.to_file(f, generate_junit_report(failed_checks), prettyprint=False)
+            TestSuite.to_file(f, generate_junit_report(checks), prettyprint=True)
         sys.exit(-1)
     else:
         print("All Policy Checks Succeeded!")
